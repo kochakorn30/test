8536d7c7a0cc6333ac6d1116f20da6f2b8af466a
kochakorn30 <kochakorn.dtk@gmail.com>
2016-11-29 16:27:31 +0700

    fc

---
+++ main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BASE COFFEE</title>
+</head>
+
+<style>
+
+.button {
+	background-color: #A9A9B2;
+    border: solid;
+    border-color:#75757B;
+    color: white;
+    padding: 25px 42px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 25px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+	
+</style>
+
+<body style="background-color: #000000; color: #FFFFFF; font-family: arial;">
+
+<div class="container" style="background-color: #2E2E2F; width: 1300px; height: 700px; margin-top: 15px; margin-left: 15px;">	
+	<div class="name" style="font-size:55px; padding-top: 200px;">
+		<b><center>BASE COFFEE</center></b>
+		<br>
+		<a href="file:///C:/xampp/htdocs/test/test/menu.php">
+		<center><button class="button"><b>LOG-IN</b></button></center>
+		</a>
+	</div>
+</div>
+
+</body>
+</html>

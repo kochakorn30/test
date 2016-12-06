79fc6e94f48ddbf57fbe7e00841b9518aa23c368
kochakorn30 <kochakorn.dtk@gmail.com>
2016-12-06 11:30:34 +0700

    add

--- REVIEW_MOVIE/index.php
+++ REVIEW_MOVIE/index.php
@@ -1,33 +1,74 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	<title>M-HOME</title>
 </head>
-
-<style>
-	
-.container {
-	background-color: #000000;
-	width: 1400px;
-	height: 750px;
-}
-
-</style>
 
 <body>
 
 	<div class="container">
-		<div class="header">
-			
+
+		<div class="header">			
+			<center>
+				<img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Linea_M_(Logo_Metro_Medellin).png" style="height: 75px; width: 75px; margin-top: 20px; margin-left: 10px;">
+				<form>								
+					<input type="text" name="search" placeholder="SEARCH HERE....">
+				</form>					
+			</center>					
 		</div><!--header-->
-		
+
+		<div class="menu">
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/index.php">
+				<div class="sub_menu" style="margin-left: 30px;">HOME</div>
+			</a>
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/lastest.php">
+				<div class="sub_menu">LASTEST</div>
+			</a>
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/movie.php">
+				<div class="sub_menu">MOVIE</div>
+			</a>
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/contact_us.php">
+				<div class="sub_menu">CONTACT</div>
+			</a>
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/setting.php">
+				<div class="sub_menu">SETTING</div>
+			</a>
+			<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/login.php">
+				<div class="sub_menu" style="float: right; margin-right: 30px;">LOGIN</div>
+			</a>
+		</div><!--menu-->
+
 		<div class="main">
 			
 		</div><!--main-->
 
 		<div class="footer">
-			
+			<img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Linea_M_(Logo_Metro_Medellin).png" style="width: 150px; height: 150px; margin-left: 200px; margin-top: 20px;">
+			<div class="footer_menu">
+				<table>
+					<tr>
+						<th><a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/index.php" style="text-decoration: none; color: #9C9C9C;">HOME</a></th>
+						<th>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp 
+							<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/contact_us.php" style="text-decoration: none; color: #9C9C9C;">CONTACT</a></th>
+					</tr>
+					<tr>
+						<th><a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/lastest.php" style="text-decoration: none; color: #9C9C9C;">LASTEST</a></th>
+						<th>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/setting.php" style="text-decoration: none; color: #9C9C9C;">SETTING</a></th>
+					</tr>
+					<tr>
+						<th><a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/movie.php" style="text-decoration: none; color: #9C9C9C;">MOVIE</a></th>
+						<th>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							<a href="file:///C:/xampp/htdocs/test/test/REVIEW_MOVIE/login.php" style="text-decoration: none; color: #9C9C9C;">LOGIN</a></th>
+					</tr>
+				</table>
+			</div>
 		</div><!--footer-->
+
 	</div><!--container-->
 
 </body>

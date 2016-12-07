8fdeded1e858e3144b8e330f8777fff2c0b5be4e
kochakorn30 <kochakorn.dtk@gmail.com>
2016-12-07 13:12:50 +0700

    h

---
+++ REVIEW_MOVIE/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>M | HOME</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+
+<div data-role="container">
+
+  <div data-role="header" data-position="fixed" class="header">
+    <center><img src="https://myfolio.com/images/logo/256_m_black.png" style="width: 80px; height: 80px;"></center>
+    	<div class="menu">
+    		<ul>
+			  <li><a class="active" href="#home" style=" color: #000000;">Home</a></li>
+			  <li><a href="#news">News</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			  <li style="float: right;"><a href="#about">About</a></li>
+			</ul>
+    	</div><!--menu-->
+  </div><!--header-->
+
+  <div data-role="main" class="ui-content" class="main">
+    
+  </div><!--main-->
+
+  <div data-role="footer" data-position="fixed" class="footer">
+    <h1>Fixed Footer</h1>
+  </div><!--footer-->
+
+</div><!--container-->
+
+</body>
+</html>

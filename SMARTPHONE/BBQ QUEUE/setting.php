91c8ac93e553562ca770fe9968e3d3f8c19e11cb
kochakorn30 <kochakorn.dtk@gmail.com>
2016-12-15 17:29:20 +0700

    add

---
+++ SMARTPHONE/BBQ QUEUE/setting.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BBQ Q | Setting</title>
+</head>
+	<style>
+		body {
+			font-family: arial;
+			font-weight: bold;
+		}
+		.container {
+			background-image: url("https://media.carphonewarehouse.com/is/image/cpw2/iphone-6GREY?$xl-standard$");
+			width: 500px;
+			height: 600px;
+			position: fixed;
+		}
+		.background {
+			background-color: #FFFFFF;
+			width: 255px;
+			height: 454px;
+			margin-top: 73px;
+			margin-left: 106px;
+		}
+		.head_bar {
+			display: inline-block;
+			background-color: #EAEAEA;
+			width: 255px;
+			height: 40px;
+			display: inline-block;
+		}
+		.notice_btn {
+			background-color:  #EE6123;
+			padding: 40px;
+			text-align: center;
+			margin-left: 10px;
+			margin-right: 10px;
+			margin-top: 70px;
+			border-radius: 5px;
+			font-size: 25px;
+		}
+		.lang_btn {
+			background-color: #61B3DE;
+			padding: 40px;
+			text-align: center;
+			margin-left: 10px;
+			margin-right: 10px;
+			margin-top: 20px;
+			border-radius: 5px;
+			font-size: 25px;
+		}
+	</style>
+<body>
+<div class="container">
+	<div class="background">
+		<div class="head_bar">
+			<table>
+				<tr>
+					<td>
+						<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/menu.php"><img src="http://www.myiconfinder.com/uploads/iconsets/dc0fa3f38f18c9d1b4e2c99967401405-Arrow.png" style="width: 40px; height: 38px;"></a>
+					</td>
+					<td>
+						<center style="font-size: 20px;margin-left: 50px; margin-bottom: 2px;">Setting</center>
+					</td>
+				</tr>
+			</table>
+		</div><!--end head_bar-->
+		<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/noticification.php" style="text-decoration: none; color: #FFFFFF;"><div class="notice_btn">Notification</div></a>
+		<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/language.php" style="text-decoration: none; color: #FFFFFF;"><div class="lang_btn">Language</div></a>
+	</div><!--end background-->
+</div><!--end container-->
+</body>
+</html>

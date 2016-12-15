41a3dde96c628cbdc5c0052836fb371a0edb9f7f
kochakorn30 <kochakorn.dtk@gmail.com>
2016-12-15 18:05:26 +0700

    ff

---
+++ SMARTPHONE/BBQ QUEUE/language.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BBQ Q | Language</title>
+</head>
+	<style>
+		body {
+			font-family: arial;
+			font-weight: bold;
+		}
+		.container {
+			background-image: url("https://media.carphonewarehouse.com/is/image/cpw2/iphone-6GREY?$xl-standard$");
+			width: 500px;
+			height: 600px;
+			position: fixed;
+		}
+		.background {
+			background-color: #FFFFFF;
+			width: 255px;
+			height: 454px;
+			margin-top: 73px;
+			margin-left: 106px;
+		}
+		.head_bar {
+			display: inline-block;
+			background-color: #EAEAEA;
+			width: 255px;
+			height: 40px;
+			display: inline-block;
+		}
+		.field {
+			width: 245px;
+			height: 170px;
+			margin-top: 50px;
+			margin-left: 5px;
+			border-radius: 10px;
+			font-size: 20px;
+		}
+		.eng_btn {
+			padding: 5px;
+			text-align: center;
+			font-size: 25px;
+		}
+		.th_btn {
+			padding: 5px;
+			text-align: center;
+			font-size: 25px;
+		}
+	</style>
+<body>
+<div class="container">
+	<div class="background">
+		<div class="head_bar">
+			<table>
+				<tr>
+					<td>
+						<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/setting.php"><img src="http://www.myiconfinder.com/uploads/iconsets/dc0fa3f38f18c9d1b4e2c99967401405-Arrow.png" style="width: 40px; height: 38px;"></a>
+					</td>
+					<td>
+						<center style="font-size: 20px;margin-left: 43px; margin-bottom: 2px;">Language</center>
+					</td>
+				</tr>
+			</table>
+		</div><!--end head_bar-->
+		<div class="field">		
+			<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/sure_eng.php" style="text-decoration: none; color: #FFFFFF;">
+				<div class="eng_btn">
+					<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1024px-Flag_of_the_United_Kingdom.svg.png" style="width: 225px; height: 90px;">
+				</div>
+			</a>		
+			<hr>			
+			<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/sure_th.php" style="text-decoration: none; color: #FFFFFF;">
+				<div class="th_btn">
+					<img src="http://www.m-culture.go.th/th/images/news/07102015/his4.jpg" style="width: 225px; height: 90px;">
+				</div>
+			</a>
+		</div><!--end field-->
+	</div><!--end background-->
+</div><!--end container-->
+</body>
+</html>
---
+++ SMARTPHONE/BBQ QUEUE/sure_eng.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QUEUE</title>
+</head>
+	<style>
+		body {
+			font-family: arial;
+			font-weight: bold;
+		}
+		.container {
+			background-image: url("https://media.carphonewarehouse.com/is/image/cpw2/iphone-6GREY?$xl-standard$");
+			width: 500px;
+			height: 600px;
+			position: fixed;
+		}
+		.background {
+			background-color: #FFFFFF;
+			width: 255px;
+			height: 454px;
+			margin-top: 73px;
+			margin-left: 106px;
+		}
+		.field {
+			padding-top: 120px;
+			font-size: 20px;
+			margin-left: 10px;
+			margin-right: 10px;
+			font-size: 25px;
+		}
+		.yes_btn {
+			background-color: #76B852;
+			padding: 10px;
+			text-align: center;
+			margin-left: 10px;
+			margin-right: 10px;
+			margin-top: 10px;
+			border-radius: 5px;
+		}
+		.no_btn {
+			background-color: #DA5A47;
+			padding: 10px;
+			text-align: center;
+			margin-left: 10px;
+			margin-right: 10px;
+			margin-top: 10px;
+			border-radius: 5px;
+		}
+	</style>
+<body>
+<div class="container">
+	<div class="background">
+		<div class="field">
+			<center>
+				Now, your language is English.
+			</center>
+		</div><!--end field-->
+		<br>
+		<br>
+		<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/BBQ%20QUEUE/language.php" style="text-decoration: none; color: #FFFFFF;"><div class="yes_btn">Okay</div></a>
+	</div><!--end background-->
+</div><!--end container-->
+</body>
+</html>

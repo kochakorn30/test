b5dc993f2a77378cf091bce0457bfc9fa12cd850
kochakorn30 <kochakorn.dtk@gmail.com>
2016-12-16 13:54:01 +0700

    11

--- SMARTPHONE/PHONE/main.php
+++ SMARTPHONE/PHONE/main.php
@@ -15,17 +15,172 @@
 			position: fixed;
 		}
 		.background {
-			background-color: #FFFFFF;
+			background-color: #2B82AD;
 			width: 255px;
 			height: 454px;
 			margin-top: 73px;
 			margin-left: 106px;
 		}
+		.head_bar {
+			background-color: #FFFFFF;
+			width: 255px;
+			height: 40px;
+		}
+		.field {
+			width: 235px;
+			height: 394px;
+			margin-left: 10px;
+			margin-top: 10px;
+		}
+		.mom {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: -3px;
+			margin-top: -3px; 
+		}
+		.dad {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: 11px;
+			margin-top: -3px;  
+		}
+		.num3 {
+			background-color: black;
+			width: 110px;
+			height: 91px; 
+			border-radius: 5px;
+			margin-left: -3px;
+			margin-top: 5px; 
+		}
+		.num4 {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: 11px;
+			margin-top: 5px;  
+		}
+		.num5 {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: -3px;
+			margin-top: 5px;  
+		}
+		.num6 {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: 11px;
+			margin-top: 5px;  
+		}
+		.num7 {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: -3px;
+			margin-top: 5px;  
+		}
+		.num8 {
+			background-color: black;
+			width: 110px;
+			height: 91px;
+			border-radius: 5px;
+			margin-left: 11px;
+			margin-top: 5px;  
+		}
 	</style>
 <body>
 <div class="container">
 	<div class="background">
-		
+		<div class="head_bar">
+			<table>
+				<tr>
+					<td>
+						<a href="file:///C:/xampp/htdocs/test/test/SMARTPHONE/screen.php">
+							<img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-arrow-back-128.png" style="width: 40px; height: 40px; margin-left: -5px; margin-top: -3px;">
+						</a>
+					</td>
+					<td style="font-size: 38px;"><center style="margin-top: -12px; margin-left: -5px;"> | </center></td>
+					<td style="font-size: 20px;"><center style="margin-bottom: 5px;">Contact</center></td>
+				</tr>
+			</table>
+		</div><!--end head_bar-->
+		<div class="field">
+			<table>
+				<tr>
+					<td>
+						<div class="mom">
+							<a href="#">
+								<img src="C:\xampp\htdocs\test\test\SMARTPHONE\PHONE\img\mom.jpg" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+					<td>
+						<div class="dad">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="num3">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+					<td>
+						<div class="num4">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="num5">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+					<td>
+						<div class="num6">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="num7">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+					<td>
+						<div class="num8">
+							<a href="#">
+								<img src="#" style="width: 110px; height: 91px; border-radius: 5px;">
+							</a>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</div><!--end field-->
 	</div><!--end background-->
 </div><!--end container-->
 </body>
